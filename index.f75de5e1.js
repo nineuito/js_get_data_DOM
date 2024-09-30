311ff56273456241d749ea547bd792b608162fea
Oleh Chernov <nineuito@mail.com>
2024-09-30 11:07:12 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const populationElements = document.querySelectorAll(".population");
+const totalPopulationContainer = document.querySelector(".total-population");
+const avgPopulationContainer = document.querySelector(".average-population");
+const arrayContainer = [
+    ...populationElements
+].map((el)=>{
+    return Number(el.innerHTML.split(",").join(""));
+});
+const totalPopulation = arrayContainer.reduce((acc, el)=>acc + el, 0);
+const averagePopulation = Math.round(totalPopulation / arrayContainer.length);
+totalPopulationContainer.innerHTML = totalPopulation.toLocaleString();
+avgPopulationContainer.innerHTML = averagePopulation.toLocaleString();
+
+//# sourceMappingURL=index.f75de5e1.js.map
